7f1720ee35164f5bc40296435f1e30434527f9e4
Matthijs Zondervan <mzondervan@hotmail.nl>
2023-07-14 00:07:14 +0200

    start reworking the popup script

--- popup.js
+++ popup.js
@@ -1,4 +1,9 @@
-chrome.tabs.getSelected(null, function(tab) {
+chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+}, function(tabs) {
+    // and use that tab to fill in out title and url
+    var tab = tabs[0];
     var tabLink = tab.url;
     var bkg = chrome.extension.getBackgroundPage();
 
